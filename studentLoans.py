cac6bb76870047e9a251b1c2bc6fb204cbfef11e
O-Edwards <oluwatosin.edwards@gmail.com>
2017-12-16 19:57:06 -0500

    Added functionality for option 1

--- studentLoans.py
+++ studentLoans.py
@@ -1,24 +1,33 @@
 import pandas as pd
+import datetime
 
-# df = pd.DataFrame(index= [:10],columns=['Loan Names','Subsidized or Unsubsidized','Amount','Interest','Start Date'])
+df = pd.DataFrame(columns=['Loan Name','Subsidized or Unsubsidized','Amount','Interest Rate','Start Date'])
 
+def addLoan(df):
+    name = input("What is the name of the Loan: ")
+    ifSub = input("Is the loan subsidized or unsubsidized: ")
+    amount = int(input("What is the amount of the Loan: "))
+    interest = float(input("What is the interest rate on the Loan: "))
+    year= int(input("Enter the Year of the Loan:"))
+    month= int(input("Enter the month(1-12) of the Loan:"))
+    day= int(input("Enter the day of the Loan:"))
+    date = datetime.date(year,month,day)
+    loan = [{'Loan Name':name,'Subsidized or Unsubsidized':ifSub,'Amount':amount,'Interest Rate':interest,'Start Date':date}]
+    newDF=pd.DataFrame(loan)
+    df = df.append(newDF)
 
-def addLoan(name,ifSub,amount,interest,date,pandasDF):
-    loan = {'Loan Name':name,'Subsidized or Unsubsidized':ifSub,'Amount':amount,'Interest Rate':interest,'Start Date':date}
-    df.from_dict(loan)
-    print(df)
+while True:
+    print('''Enter a number to choose from the following options:
+    1: Add A Loan
+    2: Remove A Loan
+    3: Log a made payment
+    4: View Accumulated Interest
+    5: Export to Text File
+    ''')
+    choice = int(input())
 
-addLoan("Tosin","Subsidized",10000,1.89,"June 25, 1993",df)
-# while True:
-#     print('''Enter a number to choose from the following options:
-#     1: Add A Loan
-#     2: Remove A Loan
-#     3: Log a made payment
-#     4: View Accumulated Interest
-#     5: Export to Text File
-#     ''')
-#     choice = int(input())
-
-#     if choice == 1:
+    if choice == 1:
+        addLoan(df)
+    elif choice ==2:
 
         
